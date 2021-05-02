a5330bcb6da006f158ef9d01a1ec981ed3493f58
whitejh <kei02012@naver.com>
2021-05-02 16:00:17 +0900

    Store 속성 모듈화

--- vue-news/src/store/index.js
+++ vue-news/src/store/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
-import { fetchNewsList, fetchJobsList, fetchAskList } from '../api/index.js';
+import mutations from './mutations.js';
+import actions from './actions.js';
 
 Vue.use(Vuex); //Vuex는 플러그인의 형태로 제공되어서 Vue.use로 Vuex 사용
 
@@ -15,40 +16,6 @@ export const store = new Vuex.Store({
       return state.ask;
     },
   },
-  mutations: {
-    SET_NEWS(state, news) {
-      state.news = news; // actions에 있는 response.data가 news 인자로 넘어옴
-    },
-    SET_JOBS(state, jobs) {
-      state.jobs = jobs;
-    },
-    SET_ASK(state, ask) {
-      state.ask = ask;
-    },
-  },
-  actions: {
-    FETCH_NEWS(context) {
-      fetchNewsList()
-        .then((response) => {
-          context.commit('SET_NEWS', response.data);
-        })
-        .catch((error) => {
-          console.log(error);
-        });
-    },
-    FETCH_JOBS({ commit }) {
-      fetchJobsList()
-        .then(({ data }) => {
-          commit('SET_JOBS', data);
-        })
-        .catch((error) => console.log(error));
-    },
-    FETCH_ASK({ commit }) {
-      fetchAskList()
-        .then(({ data }) => {
-          commit('SET_ASK', data);
-        })
-        .catch((error) => console.log(error));
-    },
-  },
+  mutations: mutations, //앞 뒤 같으면 축약가능 -> mutations
+  actions: actions, //앞 뒤 같으면 축약가능 -> actions
 });
---
+++ vue-news/src/store/mutations.js
@@ -0,0 +1,11 @@
+export default {
+  SET_NEWS(state, news) {
+    state.news = news; // actions에 있는 response.data가 news 인자로 넘어옴
+  },
+  SET_JOBS(state, jobs) {
+    state.jobs = jobs;
+  },
+  SET_ASK(state, ask) {
+    state.ask = ask;
+  },
+};

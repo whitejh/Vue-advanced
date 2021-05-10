2b3a205b61015f5c063f018a186ac1ffe2ede6a6
whitejh <kei02012@naver.com>
2021-05-11 02:31:04 +0900

    Router Transition

--- vue-news/src/App.vue
+++ vue-news/src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div id="app">
     <ToolBar></ToolBar>
-    <router-view></router-view>
+    <transition name="page">
+      <router-view></router-view>
+    </transition>
   </div>
 </template>
 
@@ -19,6 +21,15 @@ export default {
 body {
   padding: 0;
   margin: 0;
+}
+
+/* Router Transition */
+.page-enter-active,
+.page-leave-active {
+  transition: opacity 0.5s;
+}
+.page-enter, .page-leave-to /* .fade-leave-active below version 2.1.8 */ {
+  opacity: 0;
 }
 #app {
 }
